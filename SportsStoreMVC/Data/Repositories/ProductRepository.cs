5334f08883db6f7b2e451a0bae691bc0b93115e1
Thomas Duchatelet <thomas.duchatelet.y3284@student.hogent.be>
2019-12-27 18:15:12 +0100

    Add IRepository and ProductRepository

---
+++ SportsStoreMVC/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStoreMVC.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreMVC.Data.Repositories
+{
+    public class ProductRepository : IRepository<Product>
+    {
+        private ApplicationDbContext _context;
+        private DbSet<Product> _products;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _products = context.Products;
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _products.Remove(product);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _products.AsNoTracking().ToList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _products.Include(p => p.Category).SingleOrDefault(p => p.ProductId == id);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ SportsStoreMVC/Models/Domain/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStoreMVC.Models.Domain
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T product);
+        void Delete(T product);
+        void SaveChanges();
+    }
+}
