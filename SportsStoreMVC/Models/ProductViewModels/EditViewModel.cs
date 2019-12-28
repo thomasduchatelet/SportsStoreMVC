750bb36ae366cf680a5b703cb7865f412f10544a
Thomas Duchatelet <thomas.duchatelet.y3284@student.hogent.be>
2019-12-28 16:14:17 +0100

    Added EditViewModel

---
+++ SportsStoreMVC/Models/ProductViewModels/EditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using SportsStoreMVC.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreMVC.Models.ProductViewModels
+{
+    public class EditViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool InStock { get; set; }
+        public int CategoryId { get; set; }
+
+        public EditViewModel(Product product)
+        {
+            Name = product.Name;
+            Description = product.Description;
+            Price = product.Price;
+            InStock = product.InStock;
+            CategoryId = product.Category?.CategoryId ?? 0;
+        }
+
+
+    }
+}
